7002ed7cccc6efe17e17497bf00b8e7a47bfe0f8
Ahmed Lemssiah <art3que@gmail.com>
2025-04-15 15:18:16 +0200

    -

--- src/Components/Assets/ColorMode/ColorMode.jsx
+++ src/Components/Assets/ColorMode/ColorMode.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import {
   faSun,
   faMoon,
-  faSmile,
   faThumbsUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
