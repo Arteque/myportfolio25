d8dbfb41beaf984487eed10253cf9a8022d91b7a
Ahmed Lemssiah <art3que@gmail.com>
2025-05-18 09:56:10 +0200

    Small Changes

--- src/Tools/i18n.js
+++ src/Tools/i18n.js
@@ -1,22 +1,21 @@
 import i18n from "i18next";
-import { initReactI18next, useTranslation } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import { Translation } from "react-i18next";
-
 i18n
-
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
+
   .init({
     supportedLngs: ["de", "en", "fr"],
+    // fallbackLng: "de",
     debug: false,
 
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // react already safes from xss
     },
   });
 
